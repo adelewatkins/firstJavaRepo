1e3bd68ea2f0eabb8a55375d9a7caa518f04c84a
adelewatkins <adele_watkins@hotmail.com>
2024-01-12 13:46:01 +0000

    variable for each primitive exercise

---
+++ src/first_exercise/App.java
@@ -0,0 +1,41 @@
+package first_exercise;
+
+public class App {
+
+	
+	public static void main(String[] args) {
+		
+		
+//		variable for each primitive
+		
+		char ch = 'a';
+		char ch2 = '2';
+		String greeting = "Hi, my name is ";
+		String name = "Adele";
+		boolean bool = true;
+		byte byt = 10;
+		short shorty = 300;
+		int in = 1_000_000_000;
+		long longgg = 999_000_000_000_000L;
+		float floaty = 10.45F;
+		double dub = 123.45;
+		
+		System.out.println(ch);
+		System.out.println(ch2);
+		System.out.println(greeting);
+		System.out.println(name);
+		System.out.println(bool);
+		System.out.println(byt);
+		System.out.println(shorty);
+		System.out.println(in);
+		System.out.println(longgg);
+		System.out.println(floaty);
+		System.out.println(dub);
+		
+		System.out.println(greeting + name);
+		System.out.println(ch + ch2);
+		
+		
+	}
+	
+}
